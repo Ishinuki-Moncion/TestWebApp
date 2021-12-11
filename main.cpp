4e6229986776d422c403cccfa188cb215d23e06d
Ishinuki-Moncion <angelomoncion@gmail.com>
2021-12-10 21:13:04 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int counter=0;
+
+namespace two {
+	int counter=2;
+}
+
+int main() {
+	cout << two::counter << endl;
+	cout << "It is nice to edit in Windows using VI" << endl;
+	cout <<  "This is Thomas Kahng!" << endl;
+}
